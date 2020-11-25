fec216bdb72872951dfe72329a8f0db0e695b268
vedanttttt <51362126+vedanttttt@users.noreply.github.com>
2020-11-25 18:52:38 +0530

    certificate verification website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+// import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header'
+import CandidateRecords from './components/CandidateRecords'
+import CandidateVerify from './components/CandidateVerify'
+import CandidateDetails from './components/CandidateDetails'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route path="/" exact component={Header} />
+        <Route path="/admin" component={CandidateRecords} />
+        <Route path="/verify" component={CandidateVerify} />
+        {/* <Route path="/candidate" component={CandidateDetails} /> */}
+      </div>
+    </Router>
   );
 }
 
